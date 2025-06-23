d5d56c87301d35a7b77c841447c710d658f9d182
Mir Akbar Ali Khan <mirakber4@gmail.com>
2025-06-23 06:35:42 +0530

    create separate folder for challenges

--- 01.My passengerCounterApp/index.js
+++ 01.My passengerCounterApp/index.js
@@ -4,15 +4,6 @@
 
 // console.log(count)
 
-
-// 1. Create a variable, myAge, and set its value to your age
-
-// 2. Log the myAge variable to the console
-
-// let myAge = 23
-// console.log(myAge);
-
-
 // let firstBatch = 5;
 // let secondBatch = 7;
 
@@ -20,33 +11,11 @@
 
 // console.log(count);
 
-// 1. Create two variables, myAge and humanDogRatio
-// 2. Multiply the two together and store the result in myDogAge 
-// 3. Log myDogAge to the console
-
-// let myAge = 23;
-// let humanDogRatio = 7;
-// let myDogAge = myAge * humanDogRatio;
-
-// console.log(myDogAge);
-
-
-
 // let count = 5
 // count = count + 1
 // console.log(count)
 
-// Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
-// Decrease it down to 25, and then finally increase it to 70
-// Console.log the value after each step
 
-let bonusPoints = 50;
-bonusPoints = bonusPoints + 50;
-console.log(bonusPoints);
-bonusPoints = bonusPoints - 25;
-console.log(bonusPoints);
-bonusPoints = bonusPoints + 70;
-console.log(bonusPoints);
 
 
 
