e9347b3439e0954cc6ab198b27bacd53df64bade
Mir Akbar Ali Khan <mirakber4@gmail.com>
2025-06-23 06:53:16 +0530

    use functions to write less code

---
+++ 01.My passengerCounterApp/Practice/practice.js
@@ -0,0 +1,37 @@
+// document.getElementById("count-el").innerText= 5
+
+// let count = 0
+
+// console.log(count)
+
+// let firstBatch = 5;
+// let secondBatch = 7;
+
+// let count = firstBatch + secondBatch;
+
+// console.log(count);
+
+// let count = 5
+// count = count + 1
+// console.log(count)
+
+// ✅ using functions to write less code
+// Setting up the the race 🏎 🏎 🏎
+
+function countdown() {
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
+}
+
+countdown();
+
+// GO! 🏁
+// Players are running the race 🏎 💨
+// Race is finished! 🍾
+
+// Get ready for a new race 🏎 🏎 🏎
+
+countdown();  // called it again to use less code 
--- 01.My passengerCounterApp/index.js
+++ 01.My passengerCounterApp/index.js
@@ -1,19 +1,3 @@
-// document.getElementById("count-el").innerText= 5
-
-// let count = 0
-
-// console.log(count)
-
-// let firstBatch = 5;
-// let secondBatch = 7;
-
-// let count = firstBatch + secondBatch;
-
-// console.log(count);
-
-// let count = 5
-// count = count + 1
-// console.log(count)
 
 function increment () {
     console.log("button was clicked");
