8c0c950f29fd6a5429ceb3be97eb8653939a679e
Mir Akbar Ali Khan <mirakber4@gmail.com>
2025-06-23 07:39:34 +0530

    Display the count

--- 01.My passengerCounterApp/index.js
+++ 01.My passengerCounterApp/index.js
@@ -2,12 +2,15 @@
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked (log it out)
 // change the count-el in the HTML to reflect the new count
+
 let count = 0 
+//camelCase used in variable name
+let countEl = document.getElementById("count-el") //pass in arguments
 
 function increment () {
     count = count + 1
-    console.log(count);
-    
+    countEl.innerText = count
+ 
 }
 
 
