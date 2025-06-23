75f0aef0a792c92ea7672a9a4457639d6c13ac66
Mir Akbar Ali Khan <mirakber4@gmail.com>
2025-06-23 09:57:21 +0530

    Using textContent instead of innerText to view hidden contents

--- 01.My passengerCounterApp/index.js
+++ 01.My passengerCounterApp/index.js
@@ -8,7 +8,7 @@ let countEl = document.getElementById("count-el") //pass in arguments
 let saveEl = document.getElementById("save-el")
 function increment () {
     count += 1
-    countEl.innerText = count
+    countEl.textContent = count
  
 }
 
@@ -16,9 +16,9 @@ function increment () {
 
 function save () {
     // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
-    let countStr = count + "-"
-    saveEl.innerText += countStr
+    let countStr = count + " - "
     // 3. Render the variable in the saveEl using innerText
+      saveEl.textContent += countStr
     // NB: Make sure to not delete the existing content of the paragraph
     console.log(count)
     
