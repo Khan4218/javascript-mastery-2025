afb9e9abb4edaba770d7e850b5ba9d1e6debe908
Mir Akbar Ali Khan <mirakber4@gmail.com>
2025-06-23 09:47:12 +0530

    create the save feature

--- 01.My passengerCounterApp/index.js
+++ 01.My passengerCounterApp/index.js
@@ -3,10 +3,9 @@
 // increment the count variable when the button is clicked (log it out)
 // change the count-el in the HTML to reflect the new count
 
-let count = 0 
-//camelCase used in variable name
+let count = 0 //camelCase used in variable name
 let countEl = document.getElementById("count-el") //pass in arguments
-
+let saveEl = document.getElementById("save-el")
 function increment () {
     count += 1
     countEl.innerText = count
@@ -15,9 +14,13 @@ function increment () {
 
 // 1. Create a function, save(), which logs out the count when it's called
 
-
 function save () {
-    console.log(count);
+    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    let countStr = count + "-"
+    saveEl.innerText += countStr
+    // 3. Render the variable in the saveEl using innerText
+    // NB: Make sure to not delete the existing content of the paragraph
+    console.log(count)
     
 }
 
