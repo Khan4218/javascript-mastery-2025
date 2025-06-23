db7c15c2cf9891df13934e79a5989d6537a41878
Mir Akbar Ali Khan <mirakber4@gmail.com>
2025-06-23 10:08:44 +0530

    set count to 0 when we click save

--- 01.My passengerCounterApp/index.js
+++ 01.My passengerCounterApp/index.js
@@ -15,13 +15,10 @@ function increment () {
 // 1. Create a function, save(), which logs out the count when it's called
 
 function save () {
-    // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
-    let countStr = count + " - "
-    // 3. Render the variable in the saveEl using innerText
-      saveEl.textContent += countStr
-    // NB: Make sure to not delete the existing content of the paragraph
-    console.log(count)
-    
+let countStr = count + " - ";
+ saveEl.textContent += countStr;
+ countEl.textContent = 0;
+ count = 0;
 }
 
 
