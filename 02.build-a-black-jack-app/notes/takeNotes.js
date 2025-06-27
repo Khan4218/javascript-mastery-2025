6a04d88c01c19b3b5297a41eb4041f1aa4686f16
Mir Akbar Ali Khan <mirakber4@gmail.com>
2025-06-27 15:23:31 +0530

    Array Indexes

--- 02.build-a-black-jack-app/notes/takeNotes.js
+++ 02.build-a-black-jack-app/notes/takeNotes.js
@@ -1,1 +1,12 @@
 //we use === because to be strict in the condition if we use == the condition gets manipulated
+
+//Arrays
+
+// 0 indexed
+let featuredPosts = [
+    "Check out my Netflix clone",        // 0 
+    "Here's the code for my project",    // 1
+    "I've just relaunched my portfolio"  // 2
+]
+
+console.log( featuredPosts.length ) //outPut 3
--- 02.build-a-black-jack-app/practice/practice.js
+++ 02.build-a-black-jack-app/practice/practice.js
@@ -1,29 +1,29 @@
 //Boolean Data Type 
 
-let hasDiscountCode = true;
+// let hasDiscountCode = true;
 
-function discountcode() {
-    if (hasDiscountCode) {  //instead of hasDiscountCode it is "" it will be false bcoz empty string is a falsy value
-      console.log("discount applied to food order");
-      hasDiscountCode = false
-    }else {
-        console.log("no discount code available");
+// function discountcode() {
+//     if (hasDiscountCode) {  //instead of hasDiscountCode it is "" it will be false bcoz empty string is a falsy value
+//       console.log("discount applied to food order");
+//       hasDiscountCode = false
+//     }else {
+//         console.log("no discount code available");
         
-    }
-}
+//     }
+// }
 
-discountcode();
-discountcode();
+// discountcode();
+// discountcode();
 
 
 
-console.log(4 === 3)  // flase
-console.log(5 > 2)    // true
-console.log(12 > 12)  // false
-console.log(3 < 0)    // false
-console.log(3 >= 3)   // true
-console.log(11 <= 11) // true
-console.log(3 <= 2)   // false
+// console.log(4 === 3)  // flase
+// console.log(5 > 2)    // true
+// console.log(12 > 12)  // false
+// console.log(3 < 0)    // false
+// console.log(3 >= 3)   // true
+// console.log(11 <= 11) // true
+// console.log(3 <= 2)   // false
 
 
 
@@ -37,5 +37,9 @@ let featuredPosts = [
 
 // Create an array that lists your i.e. experience, education, licenses, skills or similar
 // The items of the array should be strings
-
 let myExperience = ["Front End Developer Fresher", "USIT Recruiter at Transend It solutions"]
+//Make the following appear in log 
+//we use index to log specific items
+console.log(myExperience[0]);
+console.log(myExperience[1]);
+console.log(myExperience[2]);
