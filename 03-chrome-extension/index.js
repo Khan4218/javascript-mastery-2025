794a5ef52d0d297702d593370c548f18dbba063c
Khan4218 <mirakber4@gmail.com>
2025-07-01 13:52:58 +0530

    Refractor input btn

--- 03-chrome-extension/index.js
+++ 03-chrome-extension/index.js
@@ -1,13 +1,8 @@
 // Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
 
-function saveLead() {
-  console.log("button clicked");
-
-}
-
 
 let inputBtn = document.getElementById("input-btn")
 inputBtn.addEventListener("click", function () {
-  console.log("button clicked from Event Listener");
+  console.log("button clicked");
 
 })
