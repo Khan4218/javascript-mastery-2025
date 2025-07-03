7b7be8db5fbe5eb5db47fdbc8f898365c0adcc93
Khan4218 <mirakber4@gmail.com>
2025-07-03 16:51:36 +0530

    refractor the renderLeads() to render using param

--- 03-chrome-extension/index.js
+++ 03-chrome-extension/index.js
@@ -7,34 +7,13 @@ const deleteEl = document.getElementById("delete-btn")
 
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
-if (leadsFromLocalStorage) {
-  myLeads = leadsFromLocalStorage
-  renderLeads();
-}
-
-deleteEl.addEventListener("dblclick", function () {
-  localStorage.clear();
-  myLeads = [];
-  renderLeads();
-
-})
-
-
-inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value)
-  renderLeads();
-  inputEl.value = "";
-  localStorage.setItem("myLeads", JSON.stringify(myLeads))
-
-})
-
-function renderLeads() {
+function render(leads) {
   let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
     <li> 
-     <a href="${myLeads[i]}" target = "_blank"> 
-        ${myLeads[i]}
+     <a href="${leads[i]}" target = "_blank"> 
+        ${leads[i]}
      </a>
     </li>
     `
@@ -42,4 +21,27 @@ function renderLeads() {
   }
   ulEl.innerHTML = listItems
 }
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage
+  render(myLeads);
+}
+
+deleteEl.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+
+})
+
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value)
+  render(myLeads);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
+})
+
+
 
